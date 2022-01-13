24d76f8095b8bd638ff8932a5ffb4bffb6f3e228
Larissa Novais <larinovais54@gmail.com>
2022-01-13 13:38:06 -0300

    feat:Add a component to the app

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import PrimeiroComponente from './componentes/PrimeiroComponente';
 import SerieTop10 from './componentes/SerieTop10';
+import SerieTop6 from "./componentes/SerieTop6";
 import SerieTop7 from "./componentes/SerieTop7";
 import SerieTop8 from "./componentes/SerieTop8";
 import SerieTop9 from './componentes/SerieTop9';
+import SerieTop5 from './componentes/SerieTop5';
 
 // eslint-disable-next-line import/no-anonymous-default-export
   export default (props) => (
@@ -14,7 +16,8 @@ import SerieTop9 from './componentes/SerieTop9';
             <SerieTop9 title = "09. Maid" descricao = "A nona posição tem uma minissérie dramática sobre uma mulher que trabalha como faxineira para tentar pagar as contas e cuidar da filhinha. “Maid” conseguiu uma raridade: ela é muito elogiada pela crítica, mas também bombou, e terminou 2021 como uma das séries mais buscadas do ano no Google." />
             <SerieTop8 title = "08. Evil" descricao = "Evil junta um padre, uma psicóloga, mãe de quatro meninas, e um especialista em tecnologia bem cético para investigar eventos sobrenaturais e tentar saber se são milagres, possessões demoníacas ou frutos de problemas psiquiátricos e afins."/>
             <SerieTop7 title = "07. Wandavision" descricao = "Wandavision mostra o casal de heróis Wanda e Visão vivendo uma vida de sitcom americana. Em cada episódio eles aparecem como protagonistas de uma série de comédia de uma década diferente. É bem divertido, mas tem um mistério por trás." />
-            <SerieTop7 title = "06. Mare of the Easttown" descricao = "Essa minissérie conta a história de uma detetive de uma cidadezinha americana da Pensilvânia que investiga um assassinato e luta para manter a vida dela nos eixos"/>
+            <SerieTop6 title = "06. Mare of the Easttown" descricao = "Essa minissérie conta a história de uma detetive de uma cidadezinha americana da Pensilvânia que investiga um assassinato e luta para manter a vida dela nos eixos"/>
+            <SerieTop5 title = "05. The white lotus" descricao = "Essa comédia dramática conta em sete episódios a história da relação entre hóspedes e staff de um resort no Havaí, focando na relação entre eles durante uma semana de estadia. As situações são muito bem desenvolvidas, as tensões vão se acumulando. Você assiste sempre com aquela sensação de que algo vai dar errado."/>
           </div>
     </div>
 )
