9a4ba26dc2583b75e77fd16f28a01ca61e8fb938
Larissa Novais <larinovais54@gmail.com>
2022-01-13 12:45:35 -0300

    feat: Create a function to return the series that is in the top 9

---
+++ componentes/SerieTop9.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+    export default function SerieTop9(props) {
+    return(
+        <>
+            <h2>{props.SerieTop9}</h2>
+            <p>{props.descricao}</p>
+        </>  
+    )
+}
