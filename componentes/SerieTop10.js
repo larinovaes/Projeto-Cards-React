34c7d848a6723bed06113d64c0e74d299c8016dc
Larissa Novais <larinovais54@gmail.com>
2022-01-13 13:08:00 -0300

    refactor:change props name from series to title

--- componentes/SerieTop10.js
+++ componentes/SerieTop10.js
@@ -3,7 +3,7 @@ import react from "react";
 export default function SerieTop10(props) {
       return(
           <>
-             <h2>{props.serie}</h2>
+             <h2>{props.title}</h2>
              <p>{props.descricao}</p>
           </>
       );
