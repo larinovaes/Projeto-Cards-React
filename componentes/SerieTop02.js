8064733e58fe94abcf51dc5d331e6476f3f587e5
Larissa Novais <larinovais54@gmail.com>
2022-01-13 13:50:56 -0300

    feat: Create a function to return the series that is in the top 2

---
+++ componentes/SerieTop02.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+export default function SerieTop2(props) {
+      return(
+          <>
+             <h2>{props.title}</h2>
+             <p>{props.descricao}</p>
+          </>
+      );
+}
