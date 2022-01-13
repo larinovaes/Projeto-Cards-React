b765a45ead4999a5ecea1831fd35cf4a105c1788
Larissa Novais <larinovais54@gmail.com>
2022-01-13 13:30:01 -0300

    feat: Create a function to return the series that is in the top 6

---
+++ componentes/SerieTop6.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+export default function SerieTop6(props) {
+    return(
+        <>
+            <h2>{props.title}</h2>
+            <p>{props.descricao}</p>
+        </>
+    );
+}
